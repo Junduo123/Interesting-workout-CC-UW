f544afd460c223abac8834e75a325f3aa696a26d
Junduo Dong <39603514+Junduo123@users.noreply.github.com>
2021-03-20 19:48:32 -0400

    Update wrangling&K-means2.R

--- R/wrangling&K-means2.R
+++ R/wrangling&K-means2.R
@@ -9,9 +9,6 @@ setwd("~/Desktop/Uwaterloo/719/A06")
 
 
 data <- read.csv("Assignment6_Data.csv")
-data$`Total.Percentage` <- apply(data[colnames(data[grepl("hour.",colnames(data))])], MARGIN =1, FUN=sum)
-data$`Actual Demand` <- round(data$Total.sales * data$Total.Percentage,0)
-
 
 data_copy <- data.frame(matrix(data = NA, nrow = 1000,ncol = 24))
 for (i in seq(colnames(data[grepl("hour.",names(data))]))) {
@@ -22,6 +19,7 @@ for (i in seq(colnames(data[grepl("hour.",names(data))]))) {
   }
   colnames(data_copy)[i] <- colnames(data[grepl("hour.",names(data))])[i]
 }
+data_copy$`Total.sales` <- data$Total.sales
 
 l <- list()
 for (j in c(1:1000)) {
@@ -30,14 +28,15 @@ for (j in c(1:1000)) {
     else(l[[j]] <- "NA")
   }
   data_copy$`Stock Out At`[j] <- l[[j]][1]
-  data_copy2 <- data_copy[data_copy$`Stock Out At` != 'NA',]
-  data_copy2$`num(Stock Out At)` <- sub("hour.","",data_copy2$`Stock Out At`)
+  stock_out <- data_copy[data_copy$`Stock Out At` != 'NA' & data_copy$`Stock Out At` != "hour.24",]
+  not_stock_out <- data_copy[data_copy$`Stock Out At` == 'NA' | data_copy$`Stock Out At` == "hour.24",]
+  stock_out$`num(Stock Out At)` <- sub("hour.","",stock_out$`Stock Out At`)
 }
 
 # k-means
 set.seed(123)
 wcss = vector()
-for (i in 1:5) wcss[i] = sum(kmeans(data_copy2$`num(Stock Out At)`, i)$withinss)
+for (i in 1:5) wcss[i] = sum(kmeans(stock_out[,1:24], i)$withinss)
 plot(1:5,
      wcss,
      type = 'b',
@@ -45,4 +44,11 @@ plot(1:5,
      xlab = 'Number of clusters',
      ylab = 'WCSS')
 
-#data_copy <- cbind(data[,c(1:3,28:29)],data_copy)
+kmodel <- kmeans(stock_out[,1:24],3,nstart = 20)
+centers <- kmodel$centers
+stock_out$cluster <- kmodel$cluster
+
+for (i in seq(nrow(stock_out))) {
+  stock_out$`Actual Demand`[i] <- 
+    stock_out$Total.sales[i]/(1-sum(centers[stock_out$cluster[i],stock_out$`num(Stock Out At)`[i]:24])/100)
+  }
